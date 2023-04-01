344d612ca640d93ac09c56d74aa6a9370e5103b6
Zuzanna Olszówka <92948112+zolszowka@users.noreply.github.com>
2023-04-01 10:50:34 +0200

    Delete offline directory

--- offline/zad3.py
+++
@@ -1,7 +0,0 @@
-"""
-Mamy daną N elementową tablicę T liczb rzeczywistych, w której liczby zostały wygenerowane z pewnego rozkładu losowego. 
-Rozkład ten mamy zadany jako k przedziałów [a1, b1],[a2, b2], . . . ,[ak, bk] takich, że i-ty przedział jest wybierany z prawdopodobieństwem ci, 
-a liczba z przedziału (x ∈ [ai, bi]) jest losowana zgodnie z rozkładem jednostajnym. 
-Przedziały mogą na siebie nachodzić. Liczby ai, bi są liczbami naturalnymi ze zbioru {1, . . . , N}.
-Proszę zaimplementować funkcję SortTab(T, P) sortująca podaną tablicę i zwracająca posortowaną tablicę jako wynik.
-"""
